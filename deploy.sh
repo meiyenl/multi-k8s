b3ef6d66b39366e38b66e4cb0224a9949b5cf80e
meiyenl <meiyen88.lee@gmail.com>
2019-03-07 12:15:37 -0800

    test1

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-docker build -t meiyen88/multi-client:latest -t meiyen88/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t meiyen88/multi-server:latest -t meiyen88/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t meiyen88/multi-worker:latest -t meiyen88/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push meiyen88/multi-client:latest
-docker push meiyen88/multi-server:latest
-docker push meiyen88/multi-worker:latest
-docker push meiyen88/multi-client:$SHA
-docker push meiyen88/multi-server:$SHA
-docker push meiyen88/multi-worker:$SHA
+docker build -t multi-client:latest -t multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t multi-server:latest -t multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t multi-worker:latest -t multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push meiyen88/meiyenl:multi-client:latest
+docker push meiyen88/meiyenl:multi-server:latest
+docker push meiyen88/meiyenl:multi-worker:latest
+docker push meiyen88/meiyenl:multi-client:$SHA
+docker push meiyen88/meiyenl:multi-server:$SHA
+docker push meiyen88/meiyenl:multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=meiyen88/multi-server:$SHA
 kubectl set image deployments/client-deployment client=meiyen88/multi-client:$SHA
